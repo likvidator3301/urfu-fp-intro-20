f031ba3e575510b061df75553fab613bb70ca6f7
a.isakov <a.isakov@skbkontur.ru>
2020-03-03 20:19:24 +0500

    solved tasks from lecture01

--- src/Lecture01.hs
+++ src/Lecture01.hs
@@ -247,7 +247,7 @@ someArithmeticCalculations =
     - если n < 0, то "negative"
 -}
 tellSign :: Int -> String
-tellSign n = error "not implemented"
+tellSign n = if n > 0 then "positive" else (if n == 0 then "zero" else "negative") 
 
 {-
   `howManyDigits` возвращает количество цифр целого числа `n`:
@@ -256,7 +256,11 @@ tellSign n = error "not implemented"
     - если n >= 100, то "three-digit or more"
 -}
 howManyDigits :: Int -> String
-howManyDigits n = error "not implemented"
+howManyDigits n = if n > -10 && n < 10 
+  then "single" 
+  else (if n > -100 && n < 100 
+    then "two-digit" 
+    else "three-digit or more") 
 
 {-
   `describeNumber` возвращает полное описание целого числа, используя
@@ -267,7 +271,7 @@ howManyDigits n = error "not implemented"
     - если n >= 100, то "positive three-digit or more"
 -}
 describeNumber :: Int -> String
-describeNumber n = error "not implemented"
+describeNumber n = tellSign n ++ " " ++ howManyDigits n
 
 -- </Задачи для самостоятельного решения>
 
@@ -303,7 +307,7 @@ makeZero x =
   больших чисел.
 -}
 factorial :: Integer -> Integer
-factorial n = error "not implemented"
+factorial n = if n == 0 then 1 else n * (factorial (n - 1))
 
 {-
   На вход приходит целое число. Необходимо вернуть количество цифр:
@@ -312,7 +316,7 @@ factorial n = error "not implemented"
     - если n = 144545, то 6
 -}
 digitsCount :: Int -> Int
-digitsCount n = error "not implemented"
+digitsCount n = if n > -10 && n < 10 then 1 else 1 + (digitsCount (quot n 10)) 
 
 -- </Задачи для самостоятельного решения>
 
